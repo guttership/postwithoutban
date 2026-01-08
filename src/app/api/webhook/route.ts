98f1d3b792382cbedd4302d2fd5de08a90e3250a
guttership <guttership@users.noreply.github.com>
2026-01-08 15:29:50 +0100

    Fix build errors for Vercel deployment

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -5,7 +5,7 @@ import crypto from "crypto";
 import { sendAccessEmail } from "@/lib/email";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2025-05-28.basil",
+  apiVersion: "2025-12-15.clover",
 });
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET || "";
